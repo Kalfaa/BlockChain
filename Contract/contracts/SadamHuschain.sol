93bee1961f57bbef0697f7968deeb6bd690aa76f
Kalfa <theo92320@hotmail.fr>
2019-12-03 20:20:39 +0100

    GetWellList

--- Contract/contracts/SadamHuschain.sol
+++ Contract/contracts/SadamHuschain.sol
@@ -1,13 +1,23 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.12;
+pragma experimental ABIEncoderV2;
 
 contract SadamHuschain {
+
     struct Well {
         address id;
         uint8 score;
         string name;
         string localisation;
         string description;
-        mapping(address => uint8) voteList;
+        mapping(address => uint) voteList;
+        address[] voterList;
+    }
+
+
+    struct WellJson {
+        string name;
+        string localisation;
+        string description;
     }
 
     struct Voter {
@@ -16,26 +26,66 @@ contract SadamHuschain {
         bool voted;
     }
 
-    mapping(address => Well) public wellByAddress;
+    struct VoteList {
+        mapping(address=>uint8) voteList;
+    }
+
+    address[] public wellAddressList;
+
+
+    mapping(address => Well)  public wellByAddress;
     mapping(address => Voter) public voterByAddress;
 
     function createWell(string memory name,string memory localisation,string memory description) public   {
-        Well memory wellToto = Well(msg.sender,0,name,localisation,description);
+        address[] memory addressArray ;
+        Well memory wellToto = Well(msg.sender,0,name,localisation,description,addressArray);
         wellByAddress[msg.sender] = wellToto;
+        wellAddressList.push(msg.sender);
     }
 
-    function createVoter(string memory name) public  {
+    function createVoter(string memory name) public {
        Voter memory voter = Voter(msg.sender,name,false);
         voterByAddress[msg.sender] = voter;
     }
+
+    function getMyVoter() public returns (Voter memory) {
+        return voterByAddress[msg.sender];
+    }
+
+    function getMyWell() public returns (WellJson memory) {
+            Well memory well = wellByAddress[msg.sender];
+            return WellToWellJson(well);
+    }
+
+    function getWellList() public returns(WellJson[] memory){
+        uint len = wellAddressList.length;
+        WellJson[] memory wellList = new WellJson[](len);
+        for (uint i=0; i<wellAddressList.length; i++) {
+            wellList[i] = WellToWellJson(wellByAddress[wellAddressList[i]]);
+        }
+        return wellList;
+    }
+
+    function getWellNumber() public returns(uint256){
+        return wellAddressList.length;
+    }
+
 
     function vote(address addrWell,uint8 a ) public returns (bool){
         if (a >= 0 && a< 11){
             wellByAddress[addrWell].voteList[msg.sender] = a;
             wellByAddress[addrWell].score = a;
+            address[] storage addressList = wellByAddress[addrWell].voterList;
+            addressList.push(msg.sender);
+            wellByAddress[addrWell].voterList= addressList;
+            wellAddressList.push(msg.sender);
             return true;
         }else{
             return false;
         }
+    }
+
+    function WellToWellJson(Well memory well) private returns (WellJson memory){
+        return WellJson(well.name,well.localisation,well.description);
     }
 }
--- Contract/test/TestContractExample.js
+++ Contract/test/TestContractExample.js
@@ -1,45 +1,58 @@
-const SadamHuschain = artifacts.require("SadamHuschain")
-const truffleAssert = require("truffle-assertions")
+const SadamHuschain = artifacts.require("SadamHuschain");
+const truffleAssert = require("truffle-assertions");
 
-contract("Vote", () => {
-    let sadamInstance, id, createVoter
+contract("Vote", (accounts) => {
+    let sadamInstance, id, createVoter;
 
     before(async () => {
-        sadamInstance = await SadamHuschain.deployed()
-        createVoter = await sadamInstance.createVoter("herve")
-
-        truffleAssert.eventEmitted(createVoter, "createVoter", async (ev) => {
-            if (!ev.id) return false;
-            id = ev.id;
-            return true
-        })
+        sadamInstance = await SadamHuschain.deployed();
     });
 
-    it("Should create one house and emit an 'NewHouse' event", async () => {
-        const createHouse = await houseFactoryInstance.createHouse(55, "18 Rue Montparnasse", 62)
+    it("Should create a Voter", async () => {
+        var name = "herve";
+        await sadamInstance.createVoter(name);
 
-        truffleAssert.eventEmitted(createHouse, "NewHouse", (ev) => {
-            return ev.postalAddress === "18 Rue Montparnasse"
-        })
+        var myVoter = await sadamInstance.getMyVoter.call();
+
+        assert.equal(myVoter['name'],name);
+        assert.equal(myVoter['voted'],false);
+
     });
 
+
+    it("Should create a Well", async () => {
+        var name = "Le puit de Kader";
+        var localisation = "Une loc";
+        var description = "Un puit incroyable";
+        await sadamInstance.createWell(name,localisation,description);
+
+        var myWell = await sadamInstance.getMyWell.call();
+        assert.equal(myWell['name'],name);
+        assert.equal(myWell['description'],description);
+        assert.equal(myWell['localisation'],localisation);
+
+    });
+
+    it("Should return list", async () => {
+        var name = "Le puit de Kader";
+        var localisation = "Une loc";
+        var description = "Un puit incroyable";
+        var name2 = "Le puit de Kader22222222";
+        await sadamInstance.createWell(name,localisation,description,{from: accounts[0]});
+        var wells = await sadamInstance.getWellList.call();
+        var wellso = await sadamInstance.getWellNumber.call();
+        console.log(wells);
+        console.log(wells.length);
+        console.log(wellso);
+
+    });
+/*
     it("Should return one house created by user", async () => {
-        const houses = await houseFactoryInstance.getHousesByUser.call()
+        const houses = await houseFactoryInstance.getHousesByUser.call();
         assert.equal(
             houses.length,
             2,
             "House is not found"
         )
-    });
-
-
-    it("Should return a house by ID", async () => {
-            const house = await houseFactoryInstance.getHouse.call(houseId)
-            assert.equal(
-                house.id,
-                houseId,
-                "houseIds dont match"
-            )
-        }
-    )
+    });*/
 });
